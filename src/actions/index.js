13ef51521f3cd55be3d136e5d66d76cdb4aff359
manelgavalda <expedientmanel@gmail.com>
2017-08-10 22:47:42 +0200

    Added action Book Selected

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,7 @@
 export function selectBook(book) {
-  console.log('A book has been selected:', book.title)
+  // Returning an action (object with a type property).
+  return {
+    type: 'BOOK_SELECTED',
+    payload: book
+  }
 }
